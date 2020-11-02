a6c082f025bb989c07ee3801ba2d78642084f74d
Alex Farhadi <alex.farhadi@clearme.com>
2020-11-02 16:47:11 -0600

    Pass region to the lambda TF module

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ resource "aws_cloudwatch_event_target" "guardduty_to_slack" {
 module "guardduty_to_slack" {
   source = "terraform-aws-modules/lambda/aws"
 
+  region                                  = var.region
   function_name                           = "guardduty_to_slack"
   description                             = "Guardduty to slack lambda"
   handler                                 = "index.handler"
